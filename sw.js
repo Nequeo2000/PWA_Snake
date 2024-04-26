640ae41fed4df19fece055e56e014e3879585473
Nequeo2000 <sonnenberg-patrick@web.de>
2024-04-26 12:50:25 +0200

    Update

--- sw.js
+++ sw.js
@@ -3,16 +3,26 @@ const putInCache = async (request, response) => {
     await cache.put(request, response);
 };
 
-const cacheFirst = async (request) => {
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache) {
-        return responseFromCache;
+const fetchFirst = async (request) => {
+    let abortController = new AbortController();
+    let signal = abortController.signal;
+    setTimeout(()=>{abortController.abort();},100);
+
+    try {
+        let response = await fetch(request, {signal});
+        if(response.ok){
+            putInCache(request, response.clone());
+            return response;
+        }     
+    } catch (error) {
+        const responseFromCache = await caches.match(request);
+        if (responseFromCache) {
+            return responseFromCache;
+        }
     }
-    const responseFromNetwork = await fetch(request);
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
 };
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(cacheFirst(event.request));
+    //event.respondWith(cacheFirst(event.request));
+    event.respondWith(fetchFirst(event.request));
 });
